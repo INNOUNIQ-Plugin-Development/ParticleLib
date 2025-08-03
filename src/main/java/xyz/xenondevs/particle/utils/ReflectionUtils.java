7f0cc0ca0aec197c03d86c23005c2acd5234541b
Patrik Sobol <patrik.sobol@innouniq.com>
2025-08-03 09:43:55 +0200

    1.17.0 - Added support for 1.21.8

--- src/main/java/xyz/xenondevs/particle/utils/ReflectionUtils.java
+++ src/main/java/xyz/xenondevs/particle/utils/ReflectionUtils.java
@@ -110,7 +110,7 @@ public final class ReflectionUtils {
         v1_21_R2(Arrays.asList("1.21.2-R0.1-SNAPSHOT", "1.21.3-R0.1-SNAPSHOT")),
         v1_21_R3(Collections.singletonList("1.21.4-R0.1-SNAPSHOT")),
         v1_21_R4(Collections.singletonList("1.21.5-R0.1-SNAPSHOT")),
-        v1_21_R5(Arrays.asList("1.21.6-R0.1-SNAPSHOT", "1.21.7-R0.1-SNAPSHOT")),
+        v1_21_R5(Arrays.asList("1.21.6-R0.1-SNAPSHOT", "1.21.7-R0.1-SNAPSHOT", "1.21.8-R0.1-SNAPSHOT")),
         ;
 
         private final List<String> IBVs;
